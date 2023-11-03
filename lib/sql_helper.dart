b7fc848756a696fd5f1e3034fe330fef03f5c4e3
nazimfeni <mdnazimuddin77@gmail.com>
2023-11-03 22:31:07 +0600

    updated

--- lib/sql_helper.dart
+++ lib/sql_helper.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
   static Future<void> createTables(sql.Database database) async {
-
-    /// Here triple-quoted strings ("""...""") are used to define a multi-line string in Dart.
     await database.execute("""CREATE TABLE items(
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         title TEXT,
@@ -16,6 +14,7 @@ class SQLHelper {
 // id: the id of a item
 // title, description: name and description of your activity
 // created_at: the time that the item was created. It will be automatically handled by SQLite
+
   static Future<sql.Database> db() async {
     return sql.openDatabase(
       'dbtech.db',
@@ -26,30 +25,32 @@ class SQLHelper {
     );
   }
 
-  // Create new item
+  // Create new item (journal)
   static Future<int> createItem(String title, String? descrption) async {
     final db = await SQLHelper.db();
 
     final data = {'title': title, 'description': descrption};
-
-    /// Here we used conflictAlgorithm to avoid any kind duplication data entry.
     final id = await db.insert('items', data,
         conflictAlgorithm: sql.ConflictAlgorithm.replace);
     return id;
   }
-  // Read all items
+
+  // Read all items (journals)
   static Future<List<Map<String, dynamic>>> getItems() async {
     final db = await SQLHelper.db();
     return db.query('items', orderBy: "id");
   }
-// Read a single item by id
+
+  // Read a single item by id
+  // The app doesn't use this method but I put here in case you want to see it
   static Future<List<Map<String, dynamic>>> getItem(int id) async {
     final db = await SQLHelper.db();
     return db.query('items', where: "id = ?", whereArgs: [id], limit: 1);
   }
 
   // Update an item by id
-  static Future<int> updateItem(int id, String title, String? descrption) async {
+  static Future<int> updateItem(
+      int id, String title, String? descrption) async {
     final db = await SQLHelper.db();
 
     final data = {
@@ -63,7 +64,7 @@ class SQLHelper {
     return result;
   }
 
-  // for Delete data
+  // Delete
   static Future<void> deleteItem(int id) async {
     final db = await SQLHelper.db();
     try {
@@ -73,4 +74,3 @@ class SQLHelper {
     }
   }
 }
-//Source: https://www.dbestech.com/tutorials/flutter-sqlite-crud-example
