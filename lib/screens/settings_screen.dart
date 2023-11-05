02e0ab04cc7224b8e695bb1fc03d753de07a9d18
nazimfeni <mdnazimuddin77@gmail.com>
2023-11-05 18:05:04 +0600

    BottomNavigationBar Added

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
+  _launchURL(String url) async {
+    if (await canLaunchUrl(url as Uri)) {
+      await launchUrl(url as Uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('This is settings screen'),);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Follow Us'),
+            subtitle: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 8, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      _launchURL('https://www.facebook.com/your_facebook_page');
+                    },
+                    child: Text('Facebook'),
+                  ),
+                  SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {
+                      _launchURL('https://www.linkedin.com/your_linkedin_page');
+                    },
+                    child: Text('Linkedin'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
